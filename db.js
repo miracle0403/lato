6690333e07985d8f34352a479878cb00132e9c4b
miracle lawrence <lawrencemiracle72@gmail.com>
2019-11-24 22:33:46 +0100

    yh

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 var mysql = require('mysql');
 var server = require ('./app.js');
 
-var pool  =   mysql.createPool({
-  mysql://b5dcd57ac4ac9b:53774de9@us-cdbr-iron-east-05.cleardb.net/heroku_1aa74e66b278cbb?reconnect=true});
-
+var pool  =   mysql.createPomysql://b663c78196ecd2:dce905d2@us-cdbr-iron-east-05.cleardb.net/heroku_11328878f1ace6d?reconnect=true});
 pool.getConnection( function ( err, con ){
 	if ( err ){
 		console.log( 'no connection to pool' )
