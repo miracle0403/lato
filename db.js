81df9b2ff9faf5cbcdeb194fe533e9e6c06e8c30
Miracle Lawrence <lawrencemiracle71@gmail.com>
2020-01-01 03:57:22 +0100

    gh

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
 var mysql = require('mysql');
 var server = require ('./app.js');
 
-var pool  =   mysql.createPomysql://b663c78196ecd2:dce905d2@us-cdbr-iron-east-05.cleardb.net/heroku_11328878f1ace6d?reconnect=true});
-pool.getConnection( function ( err, con ){
-	if ( err ){
-		console.log( 'no connection to pool' )
-	}
+var ool = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'b663c78196ecd2',
+  password : 'dce905d2',
+  database : 'heroku_11328878f1ace6d'
+});
+	
+pool.connect();
+
+pool.query( 'SELECT 1 + 4 AS solution', function ( err, results, fields ){
+	if ( err ) throw err;
 	else{
-		con.query( 'SELECT 1 + 4 AS solution', function ( err, results, fields ){
-			if ( err ) throw err;
-			else{
-			console.log( 'solution is ' + results[0].solution);
-			pool.releaseConnection( con );
-			}
-		});
+		console.log( 'solution is ' + results[0].solution);
 	}
 });
+pool.end();
+
 module.exports = pool;
